8fe3fe148734177f5f0482c9384f75ccf814db9c
Andrea Tosato <dossobuono@gmail.com>
2017-01-05 10:27:39 +0100

    Connected Service aggiunto

---
+++ IoTHub.ConnectedService/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoTHub.ConnectedService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
